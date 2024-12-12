ecaecfbd22899cb5552e095442d8c642312f0076
EnesBaytekin <enesbaytekin38@gmail.com>
2024-12-12 22:24:19 +0300

    chore: add time pickers to edit todo dialog

--- app/src/main/java/com/example/flowtodo/DialogEditToDo.kt
+++ app/src/main/java/com/example/flowtodo/DialogEditToDo.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -30,17 +33,58 @@ import androidx.compose.ui.window.Dialog
 fun DialogEditToDo(
     task: Task,
     onDismissRequest: () -> Unit = { },
-    onConfirm: (Task) -> Unit = { }
+    onConfirm: (Task) -> Unit = { },
+    initialFromHour: Int = 0,
+    initialFromMinute: Int = 0,
+    initialToHour: Int = 0,
+    initialToMinute: Int = 0
 ) {
     var title by rememberSaveable { mutableStateOf(task.title) }
     var description by rememberSaveable { mutableStateOf(task.description) }
+
+    var fromHour by rememberSaveable { mutableIntStateOf(initialFromHour) }
+    var fromMinute by rememberSaveable { mutableIntStateOf(initialFromMinute) }
+    var toHour by rememberSaveable { mutableIntStateOf(initialToHour) }
+    var toMinute by rememberSaveable { mutableIntStateOf(initialToMinute) }
+
+    var showFromTimePicker by remember { mutableStateOf(false) }
+    if (showFromTimePicker) {
+        CustomTimePicker(
+            fromHour,
+            fromMinute,
+            onDismissRequest = {
+                showFromTimePicker = false
+            },
+            onTimeSelected = { hour: Int, minute: Int ->
+                showFromTimePicker = false
+                fromHour = hour
+                fromMinute = minute
+            }
+        )
+    }
+    var showToTimePicker by remember { mutableStateOf(false) }
+    if (showToTimePicker) {
+        CustomTimePicker(
+            toHour,
+            toMinute,
+            onDismissRequest = {
+                showToTimePicker = false
+            },
+            onTimeSelected = { hour: Int, minute: Int ->
+                showToTimePicker = false
+                toHour = hour
+                toMinute = minute
+            }
+        )
+    }
+
     Dialog(
         onDismissRequest = onDismissRequest
     ) {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(400.dp)
+                .height(450.dp)
         ) {
             Column(
                 verticalArrangement = Arrangement.SpaceBetween,
@@ -54,32 +98,58 @@ fun DialogEditToDo(
                     fontSize = 24.sp,
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
-                Column(
-                    modifier = Modifier
-                        .wrapContentHeight()
+                LazyColumn(
+                    modifier = Modifier.weight(1f)
                 ) {
-                    OutlinedTextField(
-                        value = title,
-                        onValueChange = { title = it },
-                        singleLine = true,
-                        label = {
-                            Text("Title")
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp)
-                    )
-                    OutlinedTextField(
-                        value = description,
-                        onValueChange = { description = it },
-                        label = {
-                            Text("Description")
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 16.dp)
-                            .height(150.dp)
-                    )
+                    item {
+                        Column(
+                            modifier = Modifier
+                                .wrapContentHeight()
+                        ) {
+                            OutlinedTextField(
+                                value = title,
+                                onValueChange = { title = it },
+                                singleLine = true,
+                                label = {
+                                    Text("Title")
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 16.dp)
+                            )
+                            OutlinedTextField(
+                                value = description,
+                                onValueChange = { description = it },
+                                label = {
+                                    Text("Description")
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 16.dp)
+                                    .height(150.dp)
+                            )
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            TimePickerButton(
+                                text = "From",
+                                hour = fromHour,
+                                minute = fromMinute,
+                                onClick = {
+                                    showFromTimePicker = true
+                                }
+                            )
+                            TimePickerButton(
+                                text = "To",
+                                hour = toHour,
+                                minute = toMinute,
+                                onClick = {
+                                    showToTimePicker = true
+                                }
+                            )
+                        }
+                    }
                 }
                 Row(
                     horizontalArrangement = Arrangement.SpaceAround,
